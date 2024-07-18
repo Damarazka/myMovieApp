7db90c395962b681ab94f2a5913ea1cb06ca4ae6
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-07-18 16:08:13 +0700

    register page

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  email: string | undefined;
+  password : string | undefined
+  confirmPassword : string | undefined
 
   constructor() { }
 
+  async register() {
+    if (this.password !== this.confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+
+    await Preferences.set({
+      key: 'user',
+      value: JSON.stringify({ email: this.email, password: this.password }),
+    });
+
+    alert('Registration successful!');
+  }
   ngOnInit() {
   }
 
