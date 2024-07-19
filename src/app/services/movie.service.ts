dcc41e9245304e17513a88837c44deaddce51a37
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-07-19 15:02:08 +0700

    get api fot throw to component

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  private apiKey: string = 'ee8f42ee0d5dbb7621efd06b4aa3f961';
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) { }
+
+  getPopularMovies(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/movie/popular?api_key=${this.apiKey}`);
+  }
+
+  getMovieDetails(movieId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}`);
+  }
+
+  searchMovies(query: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/search/movie?query=${query}&api_key=${this.apiKey}`);
+  }
+}
